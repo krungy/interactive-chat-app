a94ab850273557ff96a31b91bf9e919c3a95ba5d
krungy <furler711@gmail.com>
2025-08-05 20:40:35 +0900

    [component] 라우터 및 대시보드 css 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ChatDashboard from '@/pages/ChatDashboard';
 import '@/assets/css/base/index.scss';
 import '@/assets/css/component/button.scss';
 
@@ -8,7 +8,9 @@ const App: React.FC = () => {
   return (
     <Router>
       <div className="app">
-        app
+        <Routes>
+          <Route path="/" element={<ChatDashboard />} />
+        </Routes>
       </div>
     </Router>
   );
